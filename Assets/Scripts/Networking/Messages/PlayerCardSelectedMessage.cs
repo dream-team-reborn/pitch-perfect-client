83f84f1ab6e53b76733a68053693d7a77e8cb0f0
William Soro <william.soro@hotmail.it>
2024-01-28 15:18:47 +0100

    Fixed wrong name

--- Assets/Scripts/Networking/Messages/PlayerCardSelectedMessage.cs
+++ Assets/Scripts/Networking/Messages/PlayerCardSelectedMessage.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace PitchPerfect.Networking.Messages
 {
-    public class PlayerCardSelectedRequest : BaseMessage
+    public class PlayerCardSelectedMessage : BaseMessage
     {
         public string PlayerId => _playerId;
         private string _playerId;
@@ -12,7 +12,7 @@ namespace PitchPerfect.Networking.Messages
         private List<int> _cards;
 
 
-        public PlayerCardSelectedRequest(string playerId, List<int> cards) : base(MessageType.PlayerReady)
+        public PlayerCardSelectedMessage(string playerId, List<int> cards) : base(MessageType.PlayerReady)
         {
             _playerId = playerId;
             _cards = cards;
