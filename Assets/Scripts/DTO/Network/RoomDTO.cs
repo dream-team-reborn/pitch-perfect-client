6a994b3e35ee3f3e8bd269a422c9b5c7862a65cb
William Soro <william.soro@hotmail.it>
2024-01-28 18:52:42 +0100

    Get player from room

--- Assets/Scripts/DTO/Network/RoomDTO.cs
+++ Assets/Scripts/DTO/Network/RoomDTO.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PitchPerfect.DTO
 {
@@ -12,6 +13,18 @@ namespace PitchPerfect.DTO
 
         private List<PlayerDTO> _players;
         public List<PlayerDTO> Players => _players;
+
+        public PlayerDTO GetRoomPlayerById(string id)
+        {
+            foreach(var player in _players)
+            {
+                if(player.Id.Equals(id))
+                {
+                    return player;
+                }
+            }
+            return null;
+        }
         
         public RoomDTO(string id, string name, List<PlayerDTO> players)
         {
